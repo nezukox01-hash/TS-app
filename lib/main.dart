3d10bfcc8718fe3b3b53b6a5b0d8e6d108977f2f
nezukox01-hash <nezukox01@gmail.com>
2026-03-25 09:05:48 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'app/app_loader.dart';
-import 'app/app_shell.dart';
-import 'app/theme.dart';
-import 'storage/app_storage.dart';
 
-void main() {
-  runApp(const TsApp());
-}
-
-class TsApp extends StatelessWidget {
-  const TsApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final loader = AppLoader(AppStorage());
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: buildAppTheme(),
-      home: AppShell(
-        tasks: loader.loadTasks(),
-        notes: loader.loadNotes(),
-        reports: loader.loadReports(),
-        stats: loader.loadStats(),
-      ),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const AppLoader());
 }
